d2861ff617bc310334ba49b7eaef8a964f113299
Tom Stewart <tomstewart3107@gmail.com>
2022-02-13 14:49:30 +0900

    Add some test coverage for Identity rotations in log / exp

--- test/test_geometry.cpp
+++ test/test_geometry.cpp
@@ -9,7 +9,7 @@ TEST(Geometry, so3Exp)
     AngularVelocity so3 = Matrix<3>(0, 0.454, 0.262);
 
     Rotation SO3 = exp(so3);
-    Rotation SO3_expected = Matrix<3,3>(0.866, -0.25, 0.433, 0.25, 0.967, 0.058, -0.433, 0.058, 0.899);
+    Rotation SO3_expected = Matrix<3, 3>(0.866, -0.25, 0.433, 0.25, 0.967, 0.058, -0.433, 0.058, 0.899);
 
     for (int i = 0; i < 3; ++i)
     {
@@ -17,6 +17,32 @@ TEST(Geometry, so3Exp)
         {
             EXPECT_NEAR(SO3(i, j), SO3_expected(i, j), 1e-3);
         }
+    }
+
+    // Check the edge case when angular velocity is zero
+    so3 = Matrix<3>(0.0, 0.0, 0.0);
+    SO3 = exp(so3);
+    SO3_expected = Identity<3>();
+
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            EXPECT_NEAR(SO3(i, j), SO3_expected(i, j), 1e-3);
+        }
+    }
+}
+
+TEST(Geometry, so3Log)
+{
+    Rotation SO3 = Identity<3>();
+
+    AngularVelocity so3 = log(SO3);
+    AngularVelocity so3_expected = Zeros<3>();
+
+    for (int i = 0; i < 3; ++i)
+    {
+        EXPECT_NEAR(so3(i), so3_expected(i), 1e-3);
     }
 }
 
@@ -44,9 +70,9 @@ TEST(Geometry, se3LogExp)
 
 TEST(Geometry, AdjointConversions)
 {
-    Pose Tsb(Matrix<3,3>(-1, 0, 0, 0, 1, 0, 0, 0, -1), {4, 0.4, 0});
+    Pose Tsb(Matrix<3, 3>(-1, 0, 0, 0, 1, 0, 0, 0, -1), {4, 0.4, 0});
 
-    Twist Vb = Matrix<6>(0, 0, -2,2.8, 4, 0.0);
+    Twist Vb = Matrix<6>(0, 0, -2, 2.8, 4, 0.0);
 
     Twist Vs = adjoint(Tsb) * Vb;
     Twist Vs_expected = Matrix<6>(0, 0, 2, -2, -4, 0.0);
@@ -59,7 +85,7 @@ TEST(Geometry, AdjointConversions)
 
 TEST(Geometry, SE3Inverse)
 {
-    Pose Tsb(Matrix<3,3>(-1, 0, 0, 0, 1, 0, 0, 0, -1), {4, 0.4, 0});
+    Pose Tsb(Matrix<3, 3>(-1, 0, 0, 0, 1, 0, 0, 0, -1), {4, 0.4, 0});
 
     auto identity = Tsb.inverse() * Tsb;
 
